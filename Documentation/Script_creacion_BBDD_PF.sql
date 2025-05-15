bd94cda863162fbe3e1a5edf716461c08b5bce02
Gabriela Jiménez Conde <gabrielajimenezconde@gmail.com>
2025-05-15 14:42:43 +0200

    done

--- Documentation/Script_creacion_BBDD_PF.sql
+++ Documentation/Script_creacion_BBDD_PF.sql
@@ -4,54 +4,47 @@ CREATE TABLE disciplinas(
 );
 CREATE TABLE caballos(
     id_caballo VARCHAR(50) PRIMARY KEY, -- Es la licencia del caballo
-    nombre VARCHAR(50),
-    sexo VARCHAR(50),
-    edad INT,
-    raza VARCHAR(50),
-    nacionalidad VARCHAR(50),
-    federacion VARCHAR(50),
-    id_resultado INT REFERENCES resultados(id_resultado) -- duda de si poner el ON DELETE CASCADE 
+    nombre_caballo VARCHAR(50),
+    raza_caballo VARCHAR(50),
+    nacionalidad_caballo VARCHAR(50)
 );
 CREATE TABLE jinetes(
 	id_jinete VARCHAR(50) PRIMARY KEY, 
-    nombre VARCHAR(50),
-    sexo VARCHAR(50),
-    nacionalidad VARCHAR(50),
-    federacion VARCHAR(50),
-    id_resultado INT REFERENCES resultados(id_resultado) -- duda de si poner el ON DELETE CASCADE
-);
-CREATE TABLE pruebas(
-    id_prueba SERIAL PRIMARY KEY,
-    prueba VARCHAR(50),
-    categoría VARCHAR (50),
-    ámbito VARCHAR(50),
-    fecha DATE
-    id_disciplina INT REFERENCES disciplinas(id_disciplina)
+    nombre_jinete VARCHAR(50),
+    nacionalidad_caballo VARCHAR(50)
 );
 CREATE TABLE concursos(
     id_concurso SERIAL PRIMARY KEY,
-    nombre VARCHAR(50),
-    categoría VARCHAR(50),
-    pais VARCHAR(50),
-    provinicia VARCHAR(50),
-    localidad VARCHAR(50),
-    ámbito VARCHAR(50),
-    federacion VARCHAR(50),
+    nombre_concurso VARCHAR(50),
+    categoría_concurso VARCHAR(50),
+    pais_concurso VARCHAR(50),
+    provincia_concurso VARCHAR(50),
+    localidad_concurso VARCHAR(50),
+    ámbito_concurso VARCHAR(50),
+    federacion_concurso VARCHAR(50),
+    fecha_inicio_concurso DATE,
+    fecha_fin_concurso DATE,
+);
+CREATE TABLE pruebas(
+    id_prueba SERIAL PRIMARY KEY,
+    nombre_prueba VARCHAR(50),
+    fecha_prueba DATE
+    id_disciplina INT REFERENCES disciplinas(id_disciplina)
 );
 CREATE TABLE resultados(
     id_resultado SERIAL PRIMARY KEY,
-    puesto INT,
-    estado VARCHAR(50),
-    premio BOOLEAN, -- duda si tiene sentido, tipo true es que tiene premio y false que no tiene
-    dinero_premio INT,
     id_jinete VARCHAR(50) REFERENCES jinetes(id_jinete)
-    id_caballo VARCHAR(50) REFERENCES jinetes(id_caballo)
+    id_caballo VARCHAR(50) REFERENCES caballo(id_caballo)
     id_prueba INT REFERENCES pruebas(id_prueba)
     id_concurso INT REFERENCES concursos(id_concurso)
-)
+    puesto INT,
+    estado VARCHAR(50),
+    premio BOOLEAN, 
+    dinero_premio INT
+);
 CREATE TABLE resultados_doma(
-	id_resultado INT REFERENCES resultados(id_resultado) -- duda de si poner el ON DELETE CASCADE
-    id_prueba INT REFERENCES resultados(id_prueba) -- duda de si poner el ON DELETE CASCADE
+	id_resultado INT REFERENCES resultados(id_resultado) 
+    id_prueba INT REFERENCES resultados(id_prueba) 
     nota_juez_E INT,
     nota_juez_H INT,
     nota_juez_C INT,
@@ -60,27 +53,26 @@ CREATE TABLE resultados_doma(
     nota INT	
 );
 CREATE TABLE resultados_completo(
-	id_resultado INT REFERENCES resultados(id_resultado) -- duda de si poner el ON DELETE CASCADE
-    id_prueba INT REFERENCES resultados(id_prueba) -- duda de si poner el ON DELETE CASCADE
+	id_resultado INT REFERENCES resultados(id_resultado) 
+    id_prueba INT REFERENCES resultados(id_prueba) 
     mer VARCHAR(50),
-    puntos_doma INT,
-    tiempo_obs_cross INT,
-    tiempo_cross INT,
-    ptos_obs_salto INT,
-    ptos_tiempo_salto INT,
-    ptos_total INT,
-    estado VARCHAR(50),
-    dinero_premio INT,
-    disciplina_id INT REFERENCES disciplinas(id_disciplina) 	
+    puntos_doma FLOAT,
+    tiempo_obs_cross FLOAT,
+    tiempo_cross FLOAT,
+    ptos_obs_salto FLOAT,
+    ptos_tiempo_salto FLOAT,
+    ptos_total FLOAT,	
 );
 CREATE TABLE resultados_salto(
 	id_resultado SERIAL PRIMARY KEY,
-    puesto INT,
-    puntos_obs_r1 INT,
-    tiempo_r1 INT,
-    puntos_obs_r2 INT,
-    tiempo_r2 INT,
-    estado VARCHAR(50),
-    dinero_premio INT,
-    disciplina_id INT REFERENCES disciplinas(id_disciplina) 	
+    id_prueba INT REFERENCES resultados(id_prueba)
+    puntos_obs_r1 FLOAT,
+    puntos_tmp_r1 FLOAT,
+    tiempo_r1 FLOAT,
+    puntos_obs_r2 FLOAT,
+    puntos_tmp_r2 FLOAT,
+    tiempo_r2 FLOAT,
+    puntos_obs_r3 FLOAT,
+    puntos_tmp_r3 FLOAT,
+    tiempo_r3 FLOAT	
 );

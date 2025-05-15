dfcaa7d9e7929f710e6ed0f08ae1b962edc1be9e
Gabriela Jiménez Conde <gabrielajimenezconde@gmail.com>
2025-05-15 20:09:44 +0200

    done

--- Documentation/Script_creacion_BBDD_PF.sql
+++ Documentation/Script_creacion_BBDD_PF.sql
@@ -5,33 +5,30 @@ disciplina VARCHAR(50)
 
 CREATE TABLE caballos(
 id_caballo VARCHAR(50) PRIMARY KEY,
-nombre_caballo VARCHAR(50),
-raza_caballo VARCHAR(50),
-nacionalidad_caballo VARCHAR(50)
+nombre_caballo VARCHAR(50)
 );
 
 CREATE TABLE jinetes(
 id_jinete INT PRIMARY KEY,
-nombre_jinete VARCHAR(50),
-nacionalidad_jinete VARCHAR(50)  
+nombre_jinete VARCHAR(50) 
 );
 
 CREATE TABLE concursos(
 id_concurso SERIAL PRIMARY KEY,
-nombre_concurso VARCHAR(50),
+nombre_concurso VARCHAR(255),
 categoria_concurso VARCHAR(50),
-pais_concurso VARCHAR(50),
-provincia_concurso VARCHAR(50),
-localidad_concurso VARCHAR(50),
+pais_concurso VARCHAR(255),
+provincia_concurso VARCHAR(255),
+localidad_concurso VARCHAR(255),
 ambito_concurso VARCHAR(50),
-federacion_concurso VARCHAR(50),
+federacion_concurso VARCHAR(255),
 fecha_inicio_concurso DATE,
 fecha_fin_concurso DATE
 );
 
 CREATE TABLE pruebas(
 id_prueba SERIAL PRIMARY KEY,
-nombre_prueba VARCHAR(50),
+nombre_prueba VARCHAR(255),
 fecha_prueba DATE,
 id_disciplina INT REFERENCES disciplinas(id_disciplina)
 );
@@ -50,7 +47,7 @@ dinero_premio FLOAT
 
 
 CREATE TABLE resultados_doma(
-id_resultado INT PRIMARY KEY REFERENCES resultados(id_resultado),  
+id_resultado INT REFERENCES resultados(id_resultado) PRIMARY KEY ,  
 id_prueba INT REFERENCES resultados(id_prueba),  
 nota_juez_E INT,
 nota_juez_H INT,
